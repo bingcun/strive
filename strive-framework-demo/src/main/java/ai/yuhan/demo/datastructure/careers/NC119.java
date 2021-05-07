4c93d39ea7ee63af28cad8db85d713b6ed919c93
chenbingcun <chenbingcun>
2021-05-07 09:31:17 +0800

    提交测试

---
+++ strive-framework-demo/src/main/java/ai/yuhan/demo/datastructure/careers/NC119.java
@@ -0,0 +1,41 @@
+package ai.yuhan.demo.datastructure.careers;
+
+import java.util.ArrayList;
+
+/**
+ * @description:
+ * @author: bingcun.chen
+ * @Date: 2021/5/7 9:27
+ * @Version 1.0
+ */
+public class NC119 {
+
+    /**
+     * 题目描述
+     * 给定一个数组，找出其中最小的K个数。例如数组元素是4,5,1,6,2,7,3,8这8个数字，则最小的4个数字是1,2,3,4。如果K>数组的长度，那么返回一个空的数组
+     *
+     *示例1
+     * 输入
+     * [4,5,1,6,2,7,3,8],4
+     * 返回值
+     * [1,2,3,4]
+     * @param input
+     * @param k
+     * @return
+     */
+    public ArrayList<Integer> GetLeastNumbers_Solution(int [] input, int k) {
+
+
+
+
+
+
+
+        return null;
+    }
+
+
+
+
+
+}
