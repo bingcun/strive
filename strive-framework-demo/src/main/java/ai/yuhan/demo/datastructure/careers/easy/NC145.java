f40e100f20fcfb3652643961bce7c7d198f8eb6d
chenbingcun <chenbingcun>
2021-05-17 18:36:18 +0800

    提交内容: 1.推送新算法接口到github

---
+++ strive-framework-demo/src/main/java/ai/yuhan/demo/datastructure/careers/easy/NC145.java
@@ -0,0 +1,18 @@
+package ai.yuhan.demo.datastructure.careers.easy;
+
+/**
+ * @description: 背包问题
+ * 已知一个背包最多能容纳物体的体积为V
+ * 现有n个物品第i个物品的体积为vi
+ * 第i个物品的重量为wi 求当前背包最多能装多大重量的物品
+ * 示例1
+ * 10,2,[[1,3],[10,4]]
+ * 返回
+ * 4
+ *
+ * @author: bingcun.chen
+ * @Date: 2021/5/17 16:17
+ * @Version 1.0
+ */
+public class NC145 {
+}
